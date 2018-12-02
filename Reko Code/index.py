8791dbfc9775c8d004e4e1287b5b57b0255e4511
kradanfi <kunnachai@gmail.com>
2018-12-02 13:15:51 +0700

    add success reko

--- Reko Code/index.py
+++ Reko Code/index.py
@@ -1,24 +1,45 @@
+import json
+import urllib
 import boto3
+import time
+import logging
+import os
+import requests
+#from botocore.vendored import requests
+from datetime import datetime
 
-BUCKET = "onepic4emer"
-KEY = "fire01.JPEG"
-
-def detect_labels(bucket, key, max_labels=10, min_confidence=50, region="us-east-1"):
+def detect_labels( max_labels=10, min_confidence=50, region="us-east-1"):
 	rekognition = boto3.client("rekognition", region)
+	headers ={'Content-Type': 'application/json','Authorization': 'Bearer 9JxmzOjA8c41FejOXkG708mK7fjFR6NQPCYOGfzrg3hAUbKpsULDkj4TLTF+R1m5si/AXQVx1RrwvjktI+jkSFV6ii+ERsobrWr8fvSuOshgCi61zuthnzVUjdOxBy/kk+oOLZd2C/0FSDXCRz8lFQdB04t89/1O/w1cDnyilFU='}
+	response = requests.get('https://api.line.me/v2/bot/message/8945907941044/content',headers=headers)
+	response_context = response.content
 	response = rekognition.detect_labels(
-		Image={
-			"S3Object": {
-				"Bucket": bucket,
-				"Name": key,
-			}
-		},
-		MaxLabels=max_labels,
-		MinConfidence=min_confidence,
+		Image={'Bytes':  response_context }
 	)
+	print response['Labels'][0]['Name']
 	return response['Labels']
 
 def lambda_handler(event, context):
     # TODO implement
+    #BUCKET = event['Records'][0]['s3']['bucket']['name']
+    #KEY = event['Records'][0]['s3']['object']['key']
+    #print event['Records'][0]
+    #dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+    #table = dynamodb.Table('alertmessage')
+    #detect_labels()
+    '''
     for label in detect_labels(BUCKET, KEY):
-        print "{Name} - {Confidence}%".format(**label)
+    	#print "{Name} - {Confidence}%".format(**label)
+    	print label['Name']
+    	response = table.put_item(Item={'word':label['Name'],'location':"kbtg"})
+    '''
+    message = {"name": "fire"
+        ,"location":"kbtg"
+    }
+    client = boto3.client('sns')
+    response = client.publish(
+    	TargetArn='arn:aws:sns:us-east-1:590113514737:alert',
+    	Message=json.dumps({'default': json.dumps(message)}),
+    	MessageStructure='json'
+    )
     
